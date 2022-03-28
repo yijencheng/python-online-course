e3c63334b899624709fcdf3e028382173b700a59
Ivan Cheng <ivan.chengyj@shopee.com>
2022-03-28 16:59:34 +0800

    add comment

--- flask-example-app/app.py
+++ flask-example-app/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
@@ -5,7 +6,7 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
+#Warning: you should either name the file "app.py", or you shoud provide FLASK_APP env. 
 @app.route("/api/test", methods=["GET", "POST"])
 def test():
     return {
